4e4fefa3b890eb6672f03a238e2282e30515b3a1
polyglotyeoja <laoneal@uh.edu>
2016-03-21 10:51:52 -0500

    Create stack.c

---
+++ stack.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>  /* for dynamic allocation */
+#include "stack.h"  
+
+
+
+void StackInit(stackT *stackP, int maxSize){
+  stackElementT *newContents;
+  
+  newContents = (stackElement *)malloc(sizeof(stackElementT) * maxSize);
+  
+  if (newContents == NULL) {
+    fprintf(stderr, "Insufficient memory to initialize stack. \n");
+    exit(1);
+  }
+  
+  stackP->contents = newContents;
+  stackP->maxSize = maxSize;
+  stackP->top = -1;
+  
+}
