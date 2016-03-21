e3a3baf21374a5a226ce1f6d228f0fee7024e7bf
polyglotyeoja <laoneal@uh.edu>
2016-03-21 12:54:38 -0500

    Update stack.c

--- stack.c
+++ stack.c
@@ -40,5 +40,27 @@ int StackIsFull(stackT *stackP)
   return stackP->top >= stackP->maxSize - 1;
 }
 
+void StackPush(stackT *stackP, stackElementT element){
+  if(StackIsFull(stackP)){
+    fprintf(stderr, "Can't push element on stack: stack is full. \n");
+    exit(1);
+  }
+  
+  /* Put information in array; update top. */
+  
+  stackP->content[++stackP->top] = element; //having trouble understanding this line.
+  
+}
+
+stackElementT StackPop(stackT *stackP)
+{
+  if(StackIsEmpty(stackP)) {
+    fprintf(stderr, "Can't pop element from stack: stack is empty. \n");
+    exit(1);
+  }
+  
+  return stackP->contents[stackP->top--];
+}
+
 
 
