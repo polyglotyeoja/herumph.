692a647af858c9dbb61d6b0586417136f4c9acad
polyglotyeoja <laoneal@uh.edu>
2016-03-21 11:26:30 -0500

    Update stack.c

--- stack.c
+++ stack.c
@@ -6,6 +6,7 @@
 
 void StackInit(stackT *stackP, int maxSize){
   stackElementT *newContents;
+  /* Allocate a new array to hold the contents. */
   
   newContents = (stackElement *)malloc(sizeof(stackElementT) * maxSize);
   
@@ -16,11 +17,17 @@ void StackInit(stackT *stackP, int maxSize){
   
   stackP->contents = newContents;
   stackP->maxSize = maxSize;
-  stackP->top = -1;
+  stackP->top = -1; /* empty */
   
 }
 
 void StackDestroy(stackT *stackP)
 {
+  free(stackP->contents); //deallocates the memory previously allocated
   
+  stackP->contents = NULL;
+  stackP->maxSize = 0;
+  stackP->top = -1; /* empty */
 }
+
+
